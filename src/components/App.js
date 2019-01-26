18af44b4ec94ef9168c0577e93eddd0ac63089ba
Pierce Lauring <pclauring@gmail.com>
2019-01-26 12:48:13 -0500

    React-redux-pluralsight tutorial

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+//App Template to be used on everypage
+import React, { PropTypes } from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="container-fluid">
+                <p>header...</p>
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+App.propTypes = {
+    children: PropTypes.Object.isRequired
+};
+
+export default App;
---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class AboutPage extends React.Component{
+    render()
+    {
+        return (
+            <div>
+                <h1>About</h1>
+            <p>About page example text</p>
+            </div>
+        );
+    }
+}
+
+export default AboutPage;
